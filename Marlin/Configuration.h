7d733ec2b140c383849ee7e8a828a5cad9d532bc
Jan Tlamicha <shadow.to.root@gmail.com>
2022-08-29 15:19:53 +0200

    Filament out detection

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -827,7 +827,7 @@
  * Note: For Bowden Extruders make this large enough to allow load/unload.
  */
 #define PREVENT_LENGTHY_EXTRUDE
-#define EXTRUDE_MAXLENGTH 200
+#define EXTRUDE_MAXLENGTH 301
 
 //===========================================================================
 //======================== Thermal Runaway Protection =======================
@@ -1769,7 +1769,7 @@
  * RAMPS-based boards use SERVO3_PIN for the first runout sensor.
  * For other boards you may need to define FIL_RUNOUT_PIN, FIL_RUNOUT2_PIN, etc.
  */
-//#define FILAMENT_RUNOUT_SENSOR
+#define FILAMENT_RUNOUT_SENSOR
 #if ENABLED(FILAMENT_RUNOUT_SENSOR)
   #define FIL_RUNOUT_ENABLED_DEFAULT true // Enable the sensor on startup. Override with M412 followed by M500.
   #define NUM_RUNOUT_SENSORS   1          // Number of sensors, up to one per extruder. Define a FIL_RUNOUT#_PIN for each.
@@ -1823,7 +1823,7 @@
   // a feed tube. Requires 4 bytes SRAM per sensor, plus 4 bytes overhead.
   //#define FILAMENT_RUNOUT_DISTANCE_MM 25
 
-  #ifdef FILAMENT_RUNOUT_DISTANCE_MM
+  #ifdef FILAMENT_RUNOUT_DISTANCE_MM 
     // Enable this option to use an encoder disc that toggles the runout pin
     // as the filament moves. (Be sure to set FILAMENT_RUNOUT_DISTANCE_MM
     // large enough to avoid false positives.)
@@ -1906,7 +1906,7 @@
  * Turn on with the command 'M111 S32'.
  * NOTE: Requires a lot of PROGMEM!
  */
-//#define DEBUG_LEVELING_FEATURE
+#define DEBUG_LEVELING_FEATURE
 
 #if ANY(MESH_BED_LEVELING, AUTO_BED_LEVELING_UBL, PROBE_MANUALLY)
   // Set a height for the start of manual adjustment
--- openocd-scripts/ender3_stm32f103.sh
+++ openocd-scripts/ender3_stm32f103.sh
@@ -6,3 +6,12 @@ function ender3_read_512k {
 function ender3_read_256k {
     openocd -f interface/stlink.cfg -f target/stm32f1x.cfg -c init -c "reset halt" -c "flash read_bank 0 ender3_firmware_$(date).bin 0 0x1000000" -c "reset" -c shutdown
 }
+
+####
+#ender3_write <file.bin>
+function ender3_write {
+    if [ -f "$1" ]
+    then
+        openocd -f interface/stlink.cfg -f target/stm32f1x.cfg -c init -c "reset halt" -c "flash write_image erase $1 0x08000000" -c "reset" -c shutdown
+    fi
+}
